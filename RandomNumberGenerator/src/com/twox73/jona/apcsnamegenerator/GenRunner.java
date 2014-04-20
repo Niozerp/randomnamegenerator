7f1b9c20ed4a5b23fdede4effa694fc6dccd3491
Jon Ausherman <1hizapper007@gmail.com>
2014-04-20 11:23:23 -0700

    Version 6.0
    *converted console dialog to JFrame and JPanel GUI interface

--- RandomNumberGenerator/src/com/twox73/jona/apcsnamegenerator/GenRunner.java
+++ RandomNumberGenerator/src/com/twox73/jona/apcsnamegenerator/GenRunner.java
@@ -1,50 +1,101 @@
 /**
  * @author Jon Ausherman
  * @date Apr 16, 2014
- * @version 4.5
+ * @version 6.0
  */
 package com.twox73.jona.apcsnamegenerator;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 
-public class GenRunner {
+
+public class GenRunner extends JFrame {
 
 	
 	public static void main(String[] args) {
 		//some sort of loop would be used here but i'm not testing the user portion
 		//here you can add escape statements and what have you
-		ArrayList<String> gen = new ArrayList<String>();
+		final ArrayList<String> gen = new ArrayList<String>();
 		Scanner scn = new Scanner(System.in);
 		String name = "";
 		String dummy = "";
 		
-		do{
-			System.out.print("Enter a name (enter \"exit\" to finish adding names): ");
-			name = scn.nextLine();
-			if(!name.equalsIgnoreCase("exit"));{
-				dummy = name;
-				gen.add(name);
-			}
-			
-		}while(!name.equalsIgnoreCase("exit"));
+		//GUI controlls
+		final JFrame genFrame = new JFrame("Random Name Generator");
+		JPanel txP = new JPanel();
+		JPanel btP = new JPanel();
 		
-		RandomNameGenorator nameList = new RandomNameGenorator(gen);
+		//Objects to be added to txP
+		JLabel genLable = new JLabel("What do you want to do?");
 		
-		do{
-			System.out.print("enter d for default list, or m for your list (type exit to end program). ");
-			name=scn.next();
-			if(name.equalsIgnoreCase("d")){
-				System.out.println(nameList.getRanDefName());
-				System.out.print(nameList.getRanDefPhone()+"\n");
-			}else if(name.equalsIgnoreCase("m")){
-				System.out.println(nameList.getRanMyName());
-				System.out.println(nameList.getRanMyPhone()+"\n");
-			}
+		//objects to be added to btP
+		JButton addUserName = new JButton("Add a Name");
+		JButton genRanUserName = new JButton("Generate from my list");
+		JButton genRanDefName = new JButton("Genderate from the default list");
+		
+		//exti controls
+		genFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		//adds buttons to the JPanels
+		txP.add(genLable);
+		btP.add(addUserName);
+		btP.add(genRanUserName);
+		btP.add(genRanDefName);
+		
+		//adds Jpanels to the JFrame
+		genFrame.add(txP, BorderLayout.CENTER);
+		genFrame.add(btP, BorderLayout.SOUTH);
+		
+		//creates the border around the JFrame
+		genFrame.pack();
+		//makes the JFrame visible
+		genFrame.setVisible(true);
+		//centers the JFrame
+		genFrame.setLocationRelativeTo(null);
+		
+		//adds the user's name to the array list
+		addUserName.addActionListener(new ActionListener() {
 			
-		}while(!name.equalsIgnoreCase("exit"));
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				gen.add(JOptionPane.showInputDialog("Input a name"));
+				
+			}
+		});
+		
+		
+		
+	
+		final RandomNameGenorator nameList = new RandomNameGenorator(gen);
+		
+		genRanUserName.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(genFrame, nameList.getRanMyName() +"\n" +nameList.getRanMyPhone());
+				
+			}
+		});
+		
+		genRanDefName.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(genFrame, nameList.getRanDefName() +"\n"+nameList.getRanDefPhone());
+				
+			}
+		});
+		
 		
 		
 	}
--- RandomNumberGenerator/src/com/twox73/jona/apcsnamegenerator/package-info.java
+++ RandomNumberGenerator/src/com/twox73/jona/apcsnamegenerator/package-info.java
@@ -1,7 +1,7 @@
 /**
  * @author Jon Ausherman
  * @date Apr 16, 2014
- * @version 5.0
+ * @version 6.0
  * @description a name generator to select people to answer questions
  * 
  * @update: 
@@ -9,6 +9,7 @@
  * 	used once.
  * 	*removed Ryan
  * 	*added Ryan 
+ * 	*converted console input to JFrame and JPanel input and dialog
  */
 /**
  *
